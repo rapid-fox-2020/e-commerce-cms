bc37ccf11ffca0ce867e794c528060b1acae403e
Mayana <mayanafitri25@gmail.com>
2020-07-25 17:39:43 +0700

    heroku configuration

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require(`express`)
 const routes = require(`./routes`)
 const errorHandler = require(`./middlewares/errHandler`)
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
