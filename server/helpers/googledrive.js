69deb8e5819cb3da0938c0e9896dd86f9dc55dd7
stefanuslim <stefanushalim8@gmail.com>
2020-08-01 23:17:37 +0700

    hide credentials

--- server/helpers/googledrive.js
+++ server/helpers/googledrive.js
@@ -7,7 +7,16 @@ const TOKEN_PATH = 'token.json';
 
 
 function authorize() {
-  let credentials = JSON.parse(fs.readFileSync('./credentials.json','utf-8'))
+  let credentials = {
+    installed:
+    {client_id:process.env.GOOGLE_CLIENT_ID,
+    project_id:process.env.PROJECT_ID,
+    auth_uri:process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url:process.env.AUTH_PROVIDER,
+    client_secret:process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uris:[process.env.REDIRECT_URI]}}
+
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
